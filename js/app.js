e4c7928fdb0e66f6564f1e2c078fee41cd649172
Salman <you@example.com>
2022-02-17 11:46:22 +0600

    input add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-// alert('hello javascript')
+   // Input  To Value Function
+function getInputValue(inputId) {
+    let input = document.getElementById(inputId);
+    const inputValue = parseFloat(input.value);
+    input.value = ""
+    return inputValue
+}
+//  Inner Text to Numbar Function 
+function getInnerText(textId) {
+    const balanceText = document.getElementById(textId);
+    const textToNumbar = parseFloat(balanceText.innerText);
+    return textToNumbar
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    // Income Input
+    const incomeInput = getInputValue('income-input');
+
+    // Food Input 
+    const foodInput = getInputValue('food-input');
+    
+    // Rent Input 
+    const rentInput = getInputValue('rent-input');
+
+    // Clothes Input 
+    const clothesInput = getInputValue('clothes-input');
+
+    // Total Expenses
+    const sumExpenses = foodInput + rentInput + clothesInput;
+    const totalExpenses = document.getElementById('total-expenses').innerText = sumExpenses;
+
+    // Balance Total 
+    const balanceTotal = incomeInput - totalExpenses;
+    const balance = document.getElementById('balance').innerText = balanceTotal;
+    
+
+    // let totalExpensesNum = getInnerText('total-expenses');
+
+});
